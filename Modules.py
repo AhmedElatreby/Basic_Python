79dfacc3a8080ff67474d60f85cc1d773bad89d3
ahmed <ahmedsport@hotmail.com>
2021-02-18 13:02:18 +0000

    :ant: 1 :ant:

---
+++ Modules.py
@@ -0,0 +1,9 @@
+# importing claender
+import calendar
+
+# create a variable named cal
+# calender.month is a fenction take the year and month as input and retuen this calender for this month
+cal = calendar.month(2021, 2)
+
+# running this with print
+print(cal)
---
+++ greet.py
@@ -0,0 +1,6 @@
+def greet():
+    name = input("Please enter your name: ")
+    time = input("please enter the time of the day(morning/afternoon/evening):")
+    print("Good " + time + ", " + name + "!")
+
+greet()
