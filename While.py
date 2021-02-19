e4589480de8267df6dd3dae82f5e7d41325e26b8
ahmed <ahmedsport@hotmail.com>
2021-02-19 23:01:52 +0000

    :art: 2 :art:

---
+++ While.py
@@ -0,0 +1,10 @@
+vowels = ['a', 'e', 'i', 'o', 'u']
+length = len(vowels)
+print(length)
+
+print("-------------------")
+vowels = ['a', 'e', 'i', 'o', 'u']
+i = 0
+while i < len(vowels):
+    print(vowels[i])
+    i += 1
---
+++ for.py
@@ -0,0 +1,16 @@
+# using for loop to print a list of numbers 
+for i in range(5):
+    print(i)   
+     
+
+print("________________")    
+# using for loop to print oud numbers
+for i in range(1, 10, 2):
+    print(i)   
+
+print("________________")  
+
+# printing a list using for loop
+vowels = ['a', 'e', 'i', 'o', 'u']
+for v in vowels:
+    print(v)
